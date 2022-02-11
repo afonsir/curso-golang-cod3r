a5a34849cdf718d98fc3892b333984749a7cc0d8
AfonSir <afonsir17@gmail.com>
2022-02-11 20:41:56 -0300

    feat: array

---
+++ arrayslicemap/001-array/array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var grades [3]float64
+	fmt.Println(grades)
+
+	grades[0], grades[1], grades[2] = 7.8, 4.3, 9.1
+	fmt.Println(grades)
+
+	total := 0.0
+	for i := 0; i < len(grades); i++ {
+		total += grades[i]
+	}
+
+	average := total / (float64(len(grades)))
+	fmt.Printf("Média %.2f\n", average)
+}
